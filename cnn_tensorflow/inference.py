75eb854a9460fb59db81c768175b22d065d32da1
Terry <dageda1991@163.com>
2018-03-07 15:33:36 +0800

    Update inference.py

--- cnn_tensorflow/inference.py
+++ cnn_tensorflow/inference.py
@@ -3,22 +3,12 @@ import numpy as np
 import cv2
 
 MAX_CAPTCHA = 4
-CHAR_SET_LEN = 16
+# 验证码中的字符
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
 
-
-# 向量转回文本
-def vec2text(vec):
-    char_pos = vec.nonzero()[0]
-    text = []
-    for i, c in enumerate(char_pos):
-        char_idx = c % CHAR_SET_LEN
-        if char_idx < 10:
-            char_code = char_idx + ord('0')
-        else:
-            char_code = char_idx + ord('a') - 10
-        text.append(chr(char_code))
-
-    return "".join(text)
+char_set = number + alphabet
+CHAR_SET_LEN = len(char_set)
 
 
 # read and convert the image
@@ -35,21 +25,9 @@ for i in range(img_h):
         if img[i, j, 0] > 0 and img[i, j, 1] > 160:
             img_bi[i, j] = 255
 
-img_bi = img_bi.astype(np.float32)
+img_bi = img_bi.astype(np.float)
 img_bi = (img_bi - 128) / 128
-
-
-# input_blob = cv2.dnn.blobFromImage(img_bi)
-# net = cv2.dnn.readNetFromTensorflow("trained_model/frozen_model_10000.pb")
-# net.setInput(input_blob)
-# result = net.forward()
-# print(result)
-#
-#
-#
-# exit()
-
-input_blob = np.reshape(img_bi, [-1, (img_h+1) * (img_w+1)])
+input_blob = np.reshape(img_bi, [-1, img_h+1, img_w+1, 1])
 
 # read model file and inference
 with open("trained_model/frozen_model_10000.pb", "rb") as f:
@@ -58,29 +36,21 @@ with open("trained_model/frozen_model_10000.pb", "rb") as f:
     tf.import_graph_def(out_graph_def, name="")
 
     with tf.Session() as sess:
-        data = sess.graph.get_tensor_by_name("input/x_input:0")
-        predict = sess.graph.get_tensor_by_name("softmax/predict:0")
+        # 从pb文件中读取变量名
+        data = sess.graph.get_tensor_by_name("x_input:0")
+        predict = sess.graph.get_tensor_by_name("x_predict:0")
+        keep_prob = sess.graph.get_tensor_by_name("keep_prob:0")
 
-        # print(data)
-        # print(predict)
-        # print("\n")
-        # print(input_blob.shape)
-        # print(input_blob.dtype)
-        # exit()
-
+        # 开始预测
         sess.run(tf.global_variables_initializer())
-        img_out = sess.run(predict, feed_dict={data: input_blob})
-        print(img_out)
-        exit()
+        img_out = sess.run(predict, feed_dict={data: input_blob, keep_prob: 1.0})
 
         # 转化成英文字母
         out_str = ""
-        max_idx_p = tf.argmax(img_out, 2)
+        max_idx_p = np.argmax(img_out, 2)
         for i in range(max_idx_p.shape[1]):
-            tmp_str = vec2text(img_out[i])
-            # tmp_str = vec2text(img_out(max_idx_p[i]))
-            out_str.append(tmp_str)
+            tmp_str = char_set[max_idx_p[0, i]]
+            out_str += tmp_str
 
-        print(out_str)
-
-
+        print("真实值：", img_name.split("/")[1].split(".")[0])
+        print("预测值：", out_str)
