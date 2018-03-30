585433514e3666ac9d92ac7ce2da462c81c6f330
Terry <dageda1991@163.com>
2018-03-30 16:11:38 +0800

    Update train_tfrecords.py

--- cnn_tensorflow/train_tfrecords.py
+++ cnn_tensorflow/train_tfrecords.py
@@ -5,12 +5,12 @@ import os
 
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # 训练相关超参数
 BATCH_SIZE = 128
 LEARNING_RATE = 0.001
-EPOCHS = 100
+EPOCHS = 1
 
 
 # 验证码相关信息
@@ -92,8 +92,6 @@ def _parse_function(proto):
     image = tf.reshape(image, [IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS])
     image = tf.cast(image, dtype=tf.float32)  # 像素值需转换为float，后面送入卷积层参与计算
 
-
-
     # # 这里tf.cast(parsed_feature['label0'], tf.int32)得到的是tensor，放在list里面无法进行one_hot
     # image_label = [0] * MAX_CAPTCHA
     # image_label[0] = tf.cast(parsed_feature['label0'], tf.int32)
@@ -107,24 +105,14 @@ def _parse_function(proto):
     image_label2 = tf.cast(parsed_feature['label2'], tf.int32)
     image_label3 = tf.cast(parsed_feature['label3'], tf.int32)
 
+    image_label0 = tf.one_hot(image_label0, depth=CHAR_SET_LEN, axis=0)  # axis=0和1是一样的效果
+    image_label1 = tf.one_hot(image_label1, depth=CHAR_SET_LEN, axis=0)
+    image_label2 = tf.one_hot(image_label2, depth=CHAR_SET_LEN, axis=0)
+    image_label3 = tf.one_hot(image_label3, depth=CHAR_SET_LEN, axis=0)
 
-    image_label0 = tf.one_hot(image_label0, depth=CHAR_SET_LEN, axis=0)  # axis=1很重要，决定了返回值是行向量还是列向量
-    # image_label1 = tf.one_hot(image_label1, depth=CHAR_SET_LEN, axis=1)
-    # image_label2 = tf.one_hot(image_label2, depth=CHAR_SET_LEN, axis=1)
-    # image_label3 = tf.one_hot(image_label3, depth=CHAR_SET_LEN, axis=1)
+    image_label = tf.concat([image_label0, image_label1, image_label2, image_label3], axis=0)
 
-
-    # image_label = tf.concat([image_label0, image_label1, image_label2, image_label3], axis=0)
-
-
-
-    # image_label0 = tf.sparse_to_dense(parsed_feature['label0'], [MAX_CAPTCHA, CHAR_SET_LEN], 1, 0)
-    # image_label1 = tf.sparse_to_dense(parsed_feature['label1'], [MAX_CAPTCHA, CHAR_SET_LEN], 1, 0)
-    # image_label2 = tf.sparse_to_dense(parsed_feature['label2'], [MAX_CAPTCHA, CHAR_SET_LEN], 1, 0)
-    # image_label3 = tf.sparse_to_dense(parsed_feature['label3'], [MAX_CAPTCHA, CHAR_SET_LEN], 1, 0)
-    # image_label = tf.concat([image_label0, image_label1, image_label2, image_label3], axis=1)
-
-    return image, image_label0
+    return image, image_label
 
 
 def train_captcha_cnn():
@@ -132,29 +120,29 @@ def train_captcha_cnn():
     #dataset = tf.data.TFRecordDataset(filenames)
     dataset = tf.data.TFRecordDataset("captcha_train.tfrecords")
     dataset = dataset.map(_parse_function, num_parallel_calls=16).shuffle(buffer_size=10000)
-    dataset = dataset.batch(2).repeat(EPOCHS)
+    dataset = dataset.batch(BATCH_SIZE).repeat(EPOCHS)
     iter = dataset.make_initializable_iterator()
     X, Y = iter.get_next()
 
-    # # 训练相关函数
-    # output = training_network(X)    # 得到输出结果
-    # with tf.variable_scope("loss"):
-    #     loss1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 0:16], labels=Y[:, 0:16]))
-    #     loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 16:32], labels=Y[:, 16:32]))
-    #     loss3 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 32:48], labels=Y[:, 32:48]))
-    #     loss4 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 48:], labels=Y[:, 48:]))
-    #     loss = (loss1 + loss2 + loss3 + loss4) / 4.0
-    #
-    # with tf.variable_scope("train"):
-    #     optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
-    #
-    # with tf.variable_scope("accuracy"):
-    #     predict = tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN], name="x_predict")
-    #     max_idx_p = tf.argmax(predict, 2)
-    #     max_idx_l = tf.argmax(tf.reshape(Y, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), 2)
-    #
-    #     correct_pred = tf.equal(max_idx_p, max_idx_l)
-    #     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    # 训练相关函数
+    output = training_network(X)    # 得到输出结果
+    with tf.variable_scope("loss"):
+        loss1 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 0:16], labels=Y[:, 0:16]))
+        loss2 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 16:32], labels=Y[:, 16:32]))
+        loss3 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 32:48], labels=Y[:, 32:48]))
+        loss4 = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output[:, 48:], labels=Y[:, 48:]))
+        loss = (loss1 + loss2 + loss3 + loss4) / 4.0
+
+    with tf.variable_scope("train"):
+        optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss)
+
+    with tf.variable_scope("accuracy"):
+        predict = tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN], name="x_predict")
+        max_idx_p = tf.argmax(predict, 2)
+        max_idx_l = tf.argmax(tf.reshape(Y, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), 2)
+
+        correct_pred = tf.equal(max_idx_p, max_idx_l)
+        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 
     # 开始训练
@@ -171,20 +159,18 @@ def train_captcha_cnn():
                 try:
                     global_step += 1
                     sess.run([X, Y])
+                    _, loss_ = sess.run([optimizer, loss], feed_dict={keep_prob: 0.75})
 
-                    print(Y.eval())
-
-                    exit()
-                    # _, loss_ = sess.run([optimizer, loss], feed_dict={keep_prob: 0.75})
-                    # print("当前epoch：%d，训练步数：%d，损失率：%f" % (epoch, global_step, loss_))
+                    if global_step % 10 == 0:
+                        print("当前epoch：%d，训练步数：%d，损失：%f" % (epoch, global_step, loss_))
                 except tf.errors.OutOfRangeError:
                     break
 
             # 一个epoch跑完，计算此时的验证集的准确率
             sess.run(iter.initializer, feed_dict={filenames: valid_set})
             sess.run([X, Y])
-            # acc = sess.run(accuracy, feed_dict={keep_prob: 1.})
-            # print("当前epoch：%d，测试集准确率：%f" % (epoch, acc))
+            acc = sess.run(accuracy, feed_dict={keep_prob: 1.})
+            print("当前epoch：%d，测试集准确率：%f" % (epoch, acc))
 
             # 每个epoch保存一次模型
             constant_graph = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def,
